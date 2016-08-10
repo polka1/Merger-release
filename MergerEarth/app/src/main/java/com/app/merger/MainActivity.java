dcd56c40b0132776dda37956b0aafa56aae364b7
polka1 <grizzly.drum@gmail.com>
2016-08-11 02:46:43 +0300

    gr
    0.3.2

---
+++ MergerEarth/app/src/main/java/com/app/merger/MainActivity.java
@@ -0,0 +1,144 @@
+package com.app.merger;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+            ;
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    public void banner1 (View view){
+        Intent brows_link = new Intent(Intent.ACTION_VIEW, Uri.parse("https://polka1.github.io"));
+        startActivity(brows_link);
+    }
+
+    public void banner2 (View view){
+        Intent openMapActivity = new Intent(MainActivity.this, TestActivity.class);
+        startActivity(openMapActivity);
+    }
+    public void banner2_2 (MenuItem item){
+        Intent openMap = new Intent(MainActivity.this, TestActivity.class);
+        startActivity(openMap);
+    }
+    public void make_photo (MenuItem item){
+        Intent photo = new Intent(MainActivity.this, CameraActivity.class);
+        startActivity(photo);
+    }
+
+    public void signInGoogle(MenuItem item){
+        Intent signInActivity=new Intent(MainActivity.this, GoogleSignInActivity.class);
+        startActivity(signInActivity);
+
+    }
+
+/*
+    View openMap = (Button) findViewById(R.id.nav_slideshow);
+    openMap.setOnClickListener(this);
+*/
+
+    /*
+    public void openMap(View v) {
+        Intent intObj = new Intent(this, MapsActivity.class);
+        startActivity(intObj);
+    }
+*/
+}
